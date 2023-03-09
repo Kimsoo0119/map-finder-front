cebab6ff1ce1163abc310ae8dd7b231573f99a2b
unknown <illppang@naver.com>
2023-03-09 20:30:53 +0900

    Feature: 검색결과를 통해 받은 x,y 값을 위도 경도 좌표계로 변경 후 지도에 출력하도록 수정

--- src/components/MyLocationMap.tsx
+++ src/components/MyLocationMap.tsx
@@ -4,6 +4,9 @@ import { useLocation } from "react-router-dom";
 const { naver } = window;
 
 function MyLocationMap() {
+  const location = useLocation();
+  const searchedPlace = location.state;
+
   const [currentLocation, setCurrentLocation] = useState<Partial<GeolocationCoordinates>>({
     latitude: 0,
     longitude: 0,
@@ -31,7 +34,19 @@ function MyLocationMap() {
   useEffect(() => {
     const container = document.getElementById("map"); // 지도를 표시할 div
 
-    const position = new naver.maps.LatLng(currentLocation.latitude, currentLocation.longitude);
+    let position = null;
+    if (searchedPlace) {
+      // 검색한 장소가 있으면 TM128 좌표계를 위도 경도 좌표계로 변환
+      const tm128 = new naver.maps.Point(searchedPlace.mapX, searchedPlace.mapY);
+      const latlng = naver.maps.TransCoord.fromTM128ToLatLng(tm128);
+      console.log(latlng);
+
+      position = new naver.maps.LatLng(latlng.lat(), latlng.lng());
+    } else {
+      // 검색한 장소가 없으면 현재 위치로 설정
+      position = new naver.maps.LatLng(currentLocation.latitude, currentLocation.longitude);
+    }
+
     const mapOptions = {
       center: position,
       zoom: 18,
@@ -51,7 +66,12 @@ function MyLocationMap() {
     };
 
     const marker = new naver.maps.Marker(markerOptions);
-  }, [currentLocation]);
+
+    // 검색한 장소가 있으면 현재 위치 마커는 그대로 유지하면서 해당 좌표로 map의 위치를 변경
+    if (searchedPlace) {
+      map.setCenter(position);
+    }
+  }, [currentLocation, searchedPlace]);
 
   return <div id="map" style={{ width: "100%", height: "100%" }}></div>;
 }
