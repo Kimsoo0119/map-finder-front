bb290bd383e78409bd0aa72d40108897ac6c861b
unknown <illppang@naver.com>
2023-03-12 03:32:41 +0900

    Incomplete: 값이 변할때마다 지도가 재생성되는것을 막기위해 로직 수정 중..

--- src/components/MyLocationMap.tsx
+++ src/components/MyLocationMap.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const { naver } = window;
@@ -10,13 +10,16 @@ interface LocationProps {
 function MyLocationMap({ mapInit, setMapInit }: LocationProps) {
   const location = useLocation();
   const searchedPlace = location.state;
-
+  const mapRef = useRef<HTMLElement | null | any>(null);
   const [currentLocation, setCurrentLocation] = useState<Partial<GeolocationCoordinates>>({
     latitude: 0,
     longitude: 0,
   });
 
   useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      createMap(position);
+    });
     const watchId = navigator.geolocation.watchPosition(
       (position) => {
         setCurrentLocation({
@@ -28,55 +31,124 @@ function MyLocationMap({ mapInit, setMapInit }: LocationProps) {
         console.error(error);
       }
     );
-
-    // watchPosition()으로 등록한 이벤트 핸들러를 해제하기 위해 watchId를 반환하는 함수를 반환
     return () => {
       navigator.geolocation.clearWatch(watchId);
     };
   }, []);
 
-  useEffect(() => {
+  function createMap(position: GeolocationPosition) {
     const container = document.getElementById("map"); // 지도를 표시할 div
+    mapRef.current = new naver.maps.Map(container, {
+      center: naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
+    });
 
-    let position = null;
-    if (searchedPlace) {
-      // 검색한 장소가 있으면 TM128 좌표계를 위도 경도 좌표계로 변환
-      const tm128 = new naver.maps.Point(searchedPlace.mapX, searchedPlace.mapY);
-      const latlng = naver.maps.TransCoord.fromTM128ToLatLng(tm128);
-
-      position = new naver.maps.LatLng(latlng.lat(), latlng.lng());
-    } else {
-      // 검색한 장소가 없으면 현재 위치로 설정
-      position = new naver.maps.LatLng(currentLocation.latitude, currentLocation.longitude);
-    }
-
-    const mapOptions = {
-      center: position,
-      zoom: 15,
-      minZoom: 6,
-    };
-
-    const map = new naver.maps.Map(container, mapOptions);
-
-    const markerOptions = {
-      position: position,
-      map: map,
+    mapRef.current = new naver.maps.Marker({
+      position: new naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
+      map: mapRef.current,
       icon: {
         url: "https://navermaps.github.io/maps.js/docs/img/example/ico_pin.jpg",
         origin: new naver.maps.Point(0, 0),
         anchor: new naver.maps.Point(25, 26),
       },
-    };
+    });
+  }
 
-    const marker = new naver.maps.Marker(markerOptions);
-    if (mapInit) {
-      marker.setMap(null);
-    }
-    // 검색한 장소가 있으면 현재 위치 마커는 그대로 유지하면서 해당 좌표로 map의 위치를 변경
-    if (searchedPlace) {
-      map.setCenter(position);
-    }
-  }, [currentLocation, searchedPlace, mapInit]);
+  // useEffect(() => {
+  //   const container = document.getElementById("map"); // 지도를 표시할 div
+
+  //   if (searchedPlace) {
+  //     const tm128 = new naver.maps.Point(searchedPlace.mapX, searchedPlace.mapY);
+  //     const latlng = naver.maps.TransCoord.fromTM128ToLatLng(tm128);
+  //     const position = new naver.maps.LatLng(latlng.lat(), latlng.lng());
+
+  //     const mapOptions = {
+  //       center: position,
+  //       icon: {
+  //         url: "https://navermaps.github.io/maps.js/docs/img/example/ico_pin.jpg",
+  //         origin: new naver.maps.Point(0, 0),
+  //         anchor: new naver.maps.Point(25, 26),
+  //       },
+  //     };
+
+  //     const map = new naver.maps.Map(container, mapOptions);
+  //     const markerOptions = {
+  //       position: position,
+  //       map: map,
+  //       icon: {
+  //         url: "https://navermaps.github.io/maps.js/docs/img/example/ico_pin.jpg",
+  //         origin: new naver.maps.Point(0, 0),
+  //         anchor: new naver.maps.Point(25, 26),
+  //       },
+  //     };
+
+  //     new naver.maps.Marker(markerOptions);
+  //   }
+  // else {
+  //   const mapOptions = {
+  //     center: naver.maps.LatLng(currentLocation.latitude, currentLocation.longitude),
+  //     icon: {
+  //       url: "https://navermaps.github.io/maps.js/docs/img/example/ico_pin.jpg",
+  //       origin: new naver.maps.Point(0, 0),
+  //       anchor: new naver.maps.Point(25, 26),
+  //     },
+  //   };
+
+  //   const map = new naver.maps.Map(container, mapOptions);
+
+  //   const markerOptions = {
+  //     position: naver.maps.LatLng(currentLocation.latitude, currentLocation.longitude),
+  //     map: map,
+  //     icon: {
+  //       url: "https://navermaps.github.io/maps.js/docs/img/example/ico_pin.jpg",
+  //       origin: new naver.maps.Point(0, 0),
+  //       anchor: new naver.maps.Point(25, 26),
+  //     },
+  //   };
+  //   new naver.maps.Marker(markerOptions);
+  // }
+  // }, [createMap]);
+
+  // useEffect(() => {
+  //   //searchedPlace가 있을떄와 없을때를 기준으로
+  //   const container = document.getElementById("map"); // 지도를 표시할 div
+
+  //   let position = null;
+
+  //   // 검색한 장소가 없으면 현재 위치로 설정
+  //   position = new naver.maps.LatLng(currentLocation.latitude, currentLocation.longitude);
+
+  //   const mapOptions = {
+  //     icon: {
+  //       url: "https://navermaps.github.io/maps.js/docs/img/example/ico_pin.jpg",
+  //       origin: new naver.maps.Point(0, 0),
+  //       anchor: new naver.maps.Point(25, 26),
+  //     },
+  //   };
+  //   const map = new naver.maps.Map(container, mapOptions);
+
+  // const markerOptions = {
+  //   position: position,
+  //   map: map,
+  //   icon: {
+  //     url: "https://navermaps.github.io/maps.js/docs/img/example/ico_pin.jpg",
+  //     origin: new naver.maps.Point(0, 0),
+  //     anchor: new naver.maps.Point(25, 26),
+  //   },
+  // };
+
+  // const marker = new naver.maps.Marker(markerOptions);
+  //   if (mapInit) {
+  //     marker.setMap(null);
+  //   }
+  //   // 검색한 장소가 있으면 현재 위치 마커는 그대로 유지하면서 해당 좌표로 map의 위치를 변경
+  //   if (searchedPlace) {
+  //     const center = map.getCenter();
+  //     console.log(center);
+  //     console.log(position);
+
+  //     map.setCenter(position);
+  //   }
+  // }, [currentLocation, searchedPlace, mapInit]);
 
   return <div id="map" style={{ width: "100%", height: "100%" }}></div>;
 }
