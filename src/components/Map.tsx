8ecfa104e528a7046e48dfeb375a6ed02b47479a
KimSoo <ilppang@naver.com>
2023-03-23 16:14:05 +0900

    Feature: 현재 위치 버튼 구현 완료

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -15,13 +15,14 @@ function Map({ mapInit, setMapInit }: LocationProps) {
     latitude: 0,
     longitude: 0,
   });
-  const positions: any[] = [];
   const [markers, setMarkers] = useState<any[]>([]);
+  var locationBtnHtml = '<img src="/icons/my-location.svg" />';
+  var blankHtml = '<img src="/icons/blank.svg" />';
 
   useEffect(() => {
     //첫 마운트 시 현재위치를 받아오는 기능
     navigator.geolocation.getCurrentPosition((position) => {
-      createMap(position); // 현재 포지션을 기준으로 지도 생성, mapRef에 map 정보 저장 이후 수정에 용이
+      initMap(position); // 현재 포지션을 기준으로 지도 생성, mapRef에 map 정보 저장 이후 수정에 용이
     });
     //현재 위치를 계속 받아오는 기능 위치가 이동할때마다 latLon변경
     const watchId = navigator.geolocation.watchPosition(
@@ -41,18 +42,37 @@ function Map({ mapInit, setMapInit }: LocationProps) {
     };
   }, []);
 
-  function createMap(position: GeolocationPosition) {
+  function initMap(position: GeolocationPosition) {
     const container = document.getElementById("map"); // 지도를 표시할 div
     mapRef.current = new naver.maps.Map(container, {
-      center: naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
+      center: new naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
     });
-
     setMarkers([
       new naver.maps.Marker({
         position: new naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
         map: mapRef.current,
       }),
     ]);
+
+    if (mapRef.current) {
+      naver.maps.Event.once(mapRef.current, "init", function () {
+        let blank = new naver.maps.CustomControl(blankHtml, {
+          position: naver.maps.Position.LEFT_BOTTOM,
+        });
+
+        blank.setMap(mapRef.current);
+
+        let customControl = new naver.maps.CustomControl(locationBtnHtml, {
+          position: naver.maps.Position.LEFT_BOTTOM,
+        });
+        customControl.setMap(mapRef.current);
+        naver.maps.Event.addDOMListener(customControl.getElement(), "click", function () {
+          mapRef.current.setCenter(
+            new naver.maps.LatLng(position.coords.latitude, position.coords.longitude)
+          );
+        });
+      });
+    }
   }
 
   useEffect(() => {
@@ -66,7 +86,7 @@ function Map({ mapInit, setMapInit }: LocationProps) {
       const tm128 = new naver.maps.Point(searchedPlace.mapX, searchedPlace.mapY);
       const { x, y } = naver.maps.TransCoord.fromTM128ToLatLng(tm128);
 
-      if ((x != markers[1]?.position.x && y != markers[1]?.position.y) || !markers[1]) {
+      if ((x !== markers[1]?.position.x && y !== markers[1]?.position.y) || !markers[1]) {
         console.log(markers);
 
         const searchedPlaceLocation = new naver.maps.Marker({
@@ -92,6 +112,7 @@ function Map({ mapInit, setMapInit }: LocationProps) {
     };
     return latLng;
   }
+
   function updateMarker(positions: any[]) {
     try {
       positions.map((position) => {
