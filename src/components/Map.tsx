279644c80b5e6142c7f8a88c5b43fa32806bbb9d
KimSoo <ilppang@naver.com>
2023-06-11 22:47:01 +0900

    Refactor: 지도 초기화 예외처리를 위한 코드 수정

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const { naver } = window;
+
 interface LocationProps {
   mapInit: boolean;
   setMapInit: React.Dispatch<React.SetStateAction<boolean>>;
@@ -20,58 +21,65 @@ function Map({ mapInit, setMapInit }: LocationProps) {
   var blankHtml = '<img src="/icons/blank.svg" />';
 
   useEffect(() => {
-    //첫 마운트 시 현재위치를 받아오는 기능
-    navigator.geolocation.getCurrentPosition((position) => {
-      initMap(position); // 현재 포지션을 기준으로 지도 생성, mapRef에 map 정보 저장 이후 수정에 용이
-    });
-    //현재 위치를 계속 받아오는 기능 위치가 이동할때마다 latLon변경
-    const watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        setCurrentLocation({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        });
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-    return () => {
-      //마운트 해제시 watchPosition 해제
-      navigator.geolocation.clearWatch(watchId);
-    };
+    if (naver) {
+      //첫 마운트 시 현재위치를 받아오는 기능
+      navigator.geolocation.getCurrentPosition((position) => {
+        initMap(position); // 현재 포지션을 기준으로 지도 생성, mapRef에 map 정보 저장 이후 수정에 용이
+      });
+      //현재 위치를 계속 받아오는 기능 위치가 이동할때마다 latLon변경
+      const watchId = navigator.geolocation.watchPosition(
+        (position) => {
+          setCurrentLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error) => {
+          console.error("연결 에러", error);
+        }
+      );
+      return () => {
+        //마운트 해제시 watchPosition 해제
+        navigator.geolocation.clearWatch(watchId);
+      };
+    }
   }, []);
 
   function initMap(position: GeolocationPosition) {
-    const container = document.getElementById("map"); // 지도를 표시할 div
-    mapRef.current = new naver.maps.Map(container, {
-      center: new naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
-    });
-    setMarkers([
-      new naver.maps.Marker({
-        position: new naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
-        map: mapRef.current,
-      }),
-    ]);
-
-    if (mapRef.current) {
-      naver.maps.Event.once(mapRef.current, "init", function () {
-        let blank = new naver.maps.CustomControl(blankHtml, {
-          position: naver.maps.Position.LEFT_BOTTOM,
-        });
-
-        blank.setMap(mapRef.current);
-
-        let customControl = new naver.maps.CustomControl(locationBtnHtml, {
-          position: naver.maps.Position.LEFT_BOTTOM,
-        });
-        customControl.setMap(mapRef.current);
-        naver.maps.Event.addDOMListener(customControl.getElement(), "click", function () {
-          mapRef.current.setCenter(
-            new naver.maps.LatLng(position.coords.latitude, position.coords.longitude)
-          );
-        });
+    try {
+      const container = document.getElementById("map"); // 지도를 표시할 div
+      mapRef.current = new naver.maps.Map(container, {
+        center: new naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
       });
+
+      setMarkers([
+        new naver.maps.Marker({
+          position: new naver.maps.LatLng(position.coords.latitude, position.coords.longitude),
+          map: mapRef.current,
+        }),
+      ]);
+
+      if (mapRef.current) {
+        naver.maps.Event.once(mapRef.current, "init", function () {
+          let blank = new naver.maps.CustomControl(blankHtml, {
+            position: naver.maps.Position.LEFT_BOTTOM,
+          });
+
+          blank.setMap(mapRef.current);
+
+          let customControl = new naver.maps.CustomControl(locationBtnHtml, {
+            position: naver.maps.Position.LEFT_BOTTOM,
+          });
+          customControl.setMap(mapRef.current);
+          naver.maps.Event.addDOMListener(customControl.getElement(), "click", function () {
+            mapRef.current.setCenter(
+              new naver.maps.LatLng(position.coords.latitude, position.coords.longitude)
+            );
+          });
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 
