dc949c0dd71ca9faa9431a9658fa75eb0700dd4f
kimsoo0119 <illppang@naver.com>
2023-06-26 01:52:46 +0900

    Feature: redirectUri를 통해 받아온 인가코드를 백엔드로 전송하는 기능 구현 완료 #1

--- src/components/kakao/KakaoCallback.tsx
+++ src/components/kakao/KakaoCallback.tsx
@@ -1,9 +1,23 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const backEndUrl = process.env.REACT_APP_BACKEND_SERVER;
 
 export function KakaoCallBack() {
+  const [authorizationCode, setAuthorizationCode] = useState<string | null>();
+
   useEffect(() => {
     const params = new URL(document.location.toString()).searchParams;
-    console.log(params.get("code"));
+    if (params.get("code")) {
+      setAuthorizationCode(params.get("code"));
+    }
   });
+
+  useEffect(() => {
+    if (authorizationCode) {
+      axios.get(`${backEndUrl}/auth/signin/kakao?authorizationCode=${authorizationCode}`);
+    }
+  }, [authorizationCode]);
+
   return <></>;
 }
--- src/components/kakao/KakaoSignIn.tsx
+++ src/components/kakao/KakaoSignIn.tsx
@@ -3,6 +3,8 @@ const kakaoJSKey = process.env.REACT_APP_KAKAO_JAVASCRIPT_KEY;
 const { Kakao } = window;
 
 export function KakaoSignIn() {
+  const redirectUri = process.env.REACT_APP_KAKAO_REDIRECT_URL;
+
   useEffect(() => {
     if (!Kakao.isInitialized()) {
       Kakao.init(kakaoJSKey);
@@ -10,16 +12,16 @@ export function KakaoSignIn() {
   });
 
   function loginKakao() {
+    //SDK 방식 로그인
+    Kakao.Auth.authorize({
+      redirectUri,
+    });
+
     //RestAPI 방식 로그인
     // const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
     // const KAKAO_REDIRECT_URL = process.env.REACT_APP_KAKAO_REST_API_KEY;
     // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URL}`;
     // window.location.href = KAKAO_AUTH_URL;
-
-    //SDK 방식 로그인
-    Kakao.Auth.authorize({
-      redirectUri: "http://localhost:3000/kakao-oauth",
-    });
   }
 
   return <img onClick={loginKakao} src="/icons/kakao-login.png" />;
