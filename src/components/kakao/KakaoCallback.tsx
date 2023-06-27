7ab2ca5ce8f261fc476e95ce60e660859eaf7da4
kimsoo0119 <illppang@naver.com>
2023-06-28 00:56:27 +0900

    Incomplete: 인가코드를 통해 JWT 받아오도록 구현중 #1

--- src/components/kakao/KakaoCallback.tsx
+++ src/components/kakao/KakaoCallback.tsx
@@ -5,6 +5,7 @@ const backEndUrl = process.env.REACT_APP_BACKEND_SERVER;
 
 export function KakaoCallBack() {
   const [authorizationCode, setAuthorizationCode] = useState<string | null>();
+  const [token, setToken] = useState<any>();
 
   useEffect(() => {
     const params = new URL(document.location.toString()).searchParams;
@@ -15,9 +16,21 @@ export function KakaoCallBack() {
 
   useEffect(() => {
     if (authorizationCode) {
-      axios.get(`${backEndUrl}/auth/signin/kakao?authorizationCode=${authorizationCode}`);
+      getJwtToken(authorizationCode);
     }
   }, [authorizationCode]);
 
+  useEffect(() => {
+    if (token) {
+      console.log(token);
+    }
+  }, [token]);
+
+  async function getJwtToken(authorizationCode: any) {
+    const { data } = await axios.get(
+      `${backEndUrl}/auth/signin/kakao?authorizationCode=${authorizationCode}`
+    );
+    setToken(data);
+  }
   return <></>;
 }
